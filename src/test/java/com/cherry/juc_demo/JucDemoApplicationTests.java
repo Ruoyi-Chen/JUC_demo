175eb6edf3e25f6feaa96131f3f4beff59e77f6a
cherry <chenruoyisz@gmail.com>
2022-12-13 08:55:04 +0800

    Initial commit

---
+++ src/test/java/com/cherry/juc_demo/JucDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherry.juc_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JucDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
